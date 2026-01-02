6280fa97d8a3221b504bfe512eb16662f3295da2
unknown <krishna.yadav.5963@gmail.com>
2026-01-02 12:01:13 +0530

    html css created

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require("path")
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req,res) =>{
-    res.send('hello world')
+    res.sendFile(path.join(__dirname,'/public/index.html'))
 })
 app.listen(3000,()=>{
     console.log("listening at 3000")
